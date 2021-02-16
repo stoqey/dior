361e5cae67c036bd401fd5527293fface8a09015
Ceddy Muhoza <ceddymuhoza@gmail.com>
2021-02-16 02:17:40 -0500

    match UUID

--- src/OrderBook/OrderBook.test.ts
+++ src/OrderBook/OrderBook.test.ts
@@ -1,32 +1,82 @@
 import 'mocha';
 import uuid from 'uuid';
-import { expect } from 'chai';
-import { Order } from '../Order';
-import { generateUUID } from '../utils/uuid';
+import {expect} from 'chai';
+import {Order, OrderObject} from '../Order';
+import {OrderType} from '../shared';
+import {generateUUID} from '../utils/uuid';
 
 
-const symbol = "STQ";
+const symbol = 'STQ';
 
-const asks: Order[] = [
-    {
+const buyerId = generateUUID();
+
+const orderType: OrderType = 'market';
+
+const buyPrice = 11;
+const sellPrice = 10.5;
+
+const removeMinutes = (date: Date, minute: number, remove: boolean): Date => {
+    const cloneDate = new Date(date);
+    const toRemove = remove ? cloneDate.getMinutes() - minute : cloneDate.getMinutes() + minute;
+    return new Date(new Date(cloneDate.setMinutes(toRemove)));
+};
+
+// const asks: OrderObject[] = [
+//     {
+//         action: 'SELL',
+//         id: generateUUID(),
+//         instrument: symbol,
+//         clientId: buyerId,
+//         type: orderType,
+//         qty: 20,
+//         filledQty: 0,
+//         price: sellPrice,
+//         stopPrice: 0,
+//         canceled: false,
+//         date: removeMinutes(new Date, 2, true),
+//         timestamp: removeMinutes(new Date, 2, true).getTime(),
+//     },
+// ];
+
+const asksOrders: OrderObject[] = new Array(15).fill(new Date()).map((x, index) => {
+    return {
         action: 'SELL',
-        id: new uuid.v2,
+        id: generateUUID(),
         instrument: symbol,
-        clientId: string;
-        type: OrderType;
-        qty: number;
-        filledQty: number;
-        price: number;
-        stopPrice: number;
-        side: boolean;
-        canceled: boolean;
-        date: Date;
-        timestamp?: number;
-    }
-]
+        clientId: buyerId,
+        type: orderType,
+        qty: 20,
+        filledQty: 0,
+        price: sellPrice - +`0.${index}`,
+        stopPrice: 0,
+        canceled: false,
+        date: removeMinutes(new Date(), index, true),
+        timestamp: removeMinutes(new Date(), index, true).getTime(),
+    };
+});
+
+const buyOrders: OrderObject[] = new Array(15).fill(new Date()).map((x, index) => {
+    return {
+        action: 'BUY',
+        id: generateUUID(),
+        instrument: symbol,
+        clientId: buyerId,
+        type: orderType,
+        qty: 20,
+        filledQty: 0,
+        price: buyPrice - +`0.${index}`,
+        stopPrice: 0,
+        canceled: false,
+        date: removeMinutes(new Date(), index, false),
+        timestamp: removeMinutes(new Date(), index, false).getTime(),
+    };
+});
 
 describe('OrderBook', () => {
     it('should sort ask order depending on', () => {
-
-    })
-})
+        const asks = asksOrders;
+        const bids = buyOrders;
+        console.log('all orders are', {asks: JSON.stringify(asks), bids: JSON.stringify(bids)});
+        expect(asks).to.be.not.empty;
+    });
+});
