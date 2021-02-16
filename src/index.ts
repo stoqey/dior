baf6839815d1c1c46c06a3cd4835b401532eeb3b
Ceddy Muhoza <ceddymuhoza@gmail.com>
2021-02-15 20:46:22 -0500

    update todo

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,18 @@ import {Order} from './Order';
 import {OrderBook} from './OrderBook/OrderBook';
 import {Action, OrderType} from './shared';
 
-export function main() {}
+export function main() {
+    // TODO
+    // create http server
+    // create websocket server
+    // expose events for update from order book
+}
 
+/**
+ * Create a new order
+ * @param ob
+ * @param orderReq
+ */
 export const order = async (ob: OrderBook, orderReq: Order) => {
     const {options} = orderReq;
     const order = new Order(orderReq, options);
