21b1970cf42af8c4b222b1977940569f46a67357
Ceddy Muhoza <ceddymuhoza@gmail.com>
2021-02-18 23:33:31 -0500

    Get all Allorders and get orderTrackers

--- src/Order/Order.modal.ts
+++ src/Order/Order.modal.ts
@@ -1,5 +1,19 @@
 import {Model} from '@stoqey/sofa';
+import {Order} from '.';
 
 const modalName = 'Order';
+export const orderTrackerFields = ['orderId', 'type', 'action', 'price']; // for DB views
 
 export const OrderModal = new Model(modalName);
+
+export const getAllOrders = async (orderTrackers?: boolean): Promise<any[]> => {
+    try {
+        const allOrders = await OrderModal.pagination({
+            select: orderTrackers ? orderTrackerFields : '*',
+        });
+
+        return allOrders;
+    } catch (error) {
+        console.error(error);
+    }
+};
