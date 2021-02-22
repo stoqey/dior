8a9371afadc04afcff61127f1e9416da3cddd222
Ceddy Muhoza <ceddymuhoza@gmail.com>
2021-02-21 19:17:15 -0500

    Add and cancel order

--- src/clients/broker.client.ts
+++ src/clients/broker.client.ts
@@ -5,60 +5,41 @@ import {MarketDataType} from '@stoqey/client-graphql';
 
 import {log} from '../log';
 import {Currency, CurrencyModel} from '../sofa/Currency';
+import {APPEVENTS, AppEvents} from '../events';
 
 export const marketDataClient = (app: nanoexpressApp): nanoexpressApp => {
+    const events = AppEvents.Instance;
+
     // @ts-ignore
     app.get('/cancel', async function (req, res) {
-        const instrument = req.query.symbol || req.params.symbol || 'STQ';
-        // Get latest data
-        // Get top use as quote
-        try {
-            return res.json(dataToSend);
-        } catch (error) {
-            console.log('error getting quote', error);
-            res.json({});
-        }
+        const orderId = req.query || req.params.orderId;
+        events.emit(APPEVENTS.CANCEL, orderId);
+        return res.json({message: 'Successfully sent', status: 200});
     });
 
     // @ts-ignore
     app.post('/add', function (req, res) {
         const body: Order = req.body as any;
-        const {
-            stop,
-            params,
-            gtc,
-            gfd,
-            gtd,
-            action,
-            id,
-            instrument,
-            clientId,
-            type,
-            qty,
-            filledQty,
-            price,
-            stopPrice,
-            canceled,
-        } = body;
-        const newOrder: Order = new Order({
-            stop,
-            params,
-            gtc,
-            gfd,
-            gtd,
-            action,
-            id,
-            instrument,
-            clientId,
-            type,
-            qty,
-            filledQty,
-            price,
-            stopPrice,
-            canceled,
-            date: new Date(),
-        });
-        res.json(stqInfo);
+        // const {
+        //     stop,
+        //     params,
+        //     gtc,
+        //     gfd,
+        //     gtd,
+        //     action,
+        //     id,
+        //     instrument,
+        //     clientId,
+        //     type,
+        //     qty,
+        //     filledQty,
+        //     price,
+        //     stopPrice,
+        //     canceled,
+        // } = body;
+
+        events.emit(APPEVENTS.ADD, body);
+        return res.json({message: 'Successfully sent order', status: 200});
     });
 
     app.post('/update', function (req: nanoexpress.HttpRequest): nanoexpressApp {
