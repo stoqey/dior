ab7e261878da2839acc950c4cfa7a2bd1af185be
Ceddy Muhoza <ceddymuhoza@gmail.com>
2021-02-21 23:36:54 -0500

    OrderRecordModal

--- src/Order/Order.modal.ts
+++ src/Order/Order.modal.ts
@@ -2,9 +2,11 @@ import {Model} from '@stoqey/sofa';
 import {Order} from '.';
 
 const modalName = 'Order';
+const orderRecordmodalName = 'OrderRecord';
 export const orderTrackerFields = ['id', 'type', 'action', 'price']; // for DB views
 
 export const OrderModal = new Model(modalName);
+export const OrderRecordModal = new Model(orderRecordmodalName);
 
 export const getAllOrders = async (orderTrackers?: boolean): Promise<any[]> => {
     try {
