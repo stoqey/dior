6c47a2edd0d723c60db074cc49626a2334aa06d3
Ceddy Muhoza <ceddymuhoza@gmail.com>
2021-03-17 03:35:19 -0400

    getAllOrders

--- src/Order/Order.modal.ts
+++ src/Order/Order.modal.ts
@@ -3,7 +3,27 @@ import {Order} from '.';
 
 const modalName = 'Order';
 const orderRecordmodalName = 'OrderRecord';
+
 export const orderTrackerFields = ['id', 'type', 'action', 'price']; // for DB views
+export const orderFields = [
+    'id',
+    'action',
+    'instrument',
+    'symbol',
+    'clientId',
+    'type',
+    'qty',
+    'filledQty',
+    'price',
+    'stopPrice',
+    'canceled',
+    'date',
+    'workedOn',
+    'params',
+    'gtc',
+    'gfd',
+    'gtd',
+];
 
 export const OrderModal = new Model(modalName);
 export const OrderRecordModal = new Model(orderRecordmodalName);
@@ -11,7 +31,7 @@ export const OrderRecordModal = new Model(orderRecordmodalName);
 export const getAllOrders = async (orderTrackers?: boolean): Promise<any[]> => {
     try {
         const allOrders = await OrderModal.pagination({
-            select: orderTrackers ? orderTrackerFields : '*',
+            select: orderTrackers ? orderTrackerFields : orderFields,
         });
 
         return allOrders;
