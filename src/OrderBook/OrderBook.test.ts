fcc7b1a62d3a26e0c60d0f7d12060958fa51a303
Ceddy Muhoza <ceddymuhoza@gmail.com>
2021-03-26 01:15:59 -0400

    Orderbook tests

--- src/OrderBook/OrderBook.test.ts
+++ src/OrderBook/OrderBook.test.ts
@@ -1,61 +1,22 @@
 import 'mocha';
-import sum from 'lodash/sum';
 import {expect} from 'chai';
 import {Order} from '../Order';
-import {Action} from '../shared';
-import {XOrder, matchOrder} from './matching';
+import main from '../main';
+import { OrderBook } from './OrderBook';
 
-describe('BEAR: The Matching Machine LIMIT BUY+SELL', () => {
-    const market: XOrder[] = [
-        {qty: 100, price: 3.2, action: 'SELL'},
-        {qty: 100, price: 3.5, action: 'SELL'},
-        {qty: 600, price: 3.6, action: 'SELL'},
-        {qty: 100, price: 3.7, action: 'SELL'},
-        {qty: 100, price: 4.1, action: 'SELL'},
-        {qty: 50, price: 4.3, action: 'SELL'},
-        {qty: 1300, price: 5.1, action: 'SELL'},
-    ];
-
-    // BUY
-    it('it should match slice BUY order', () => {
-        const order: XOrder = {
-            qty: 300,
-            action: 'BUY',
-            price: 4.1,
-        };
-
-        const {totalFilled, orders: totalOffers} = matchOrder(order, market, true);
-
-        if (totalFilled > 0) {
-            const lastMatchedOrder = totalOffers[totalOffers.length - 1];
-            const lastMatchedOrderPrice = lastMatchedOrder[2];
-            return expect(order.price).to.be.gte(lastMatchedOrderPrice);
-        }
-    });
+before(async (done) => {
+    await main();
+    done();
 });
 
-describe('BUY: The Matching Machine LIMIT BUY+SELL', () => {
-    const market: XOrder[] = [
-        {qty: 100, price: 3.1, action: 'BUY'},
-        {qty: 100, price: 3.0, action: 'BUY'},
-        {qty: 300, price: 2.8, action: 'BUY'},
-        {qty: 50, price: 2.6, action: 'BUY'},
-        {qty: 100, price: 2.2, action: 'BUY'},
-        {qty: 300, price: 2.0, action: 'BUY'},
-        {qty: 30, price: 1.4, action: 'BUY'},
-    ];
-
+describe('OrderBook', () => {
     // BUY
-    it('it should match slice BUY order', () => {
-        const order: XOrder = {
-            qty: 300,
-            action: 'SELL',
-            price: 2.1,
-        };
+    it('it should submit an order', async () => {
+        const ob = OrderBook.app;
+        const order: Order = {
 
-        const {totalFilled, orders: totalOffers} = matchOrder(order, market, true);
-        const lastMatchedOrder = totalOffers[totalOffers.length - 1];
-        const lastMatchedOrderPrice = lastMatchedOrder[2];
-        return expect(order.price).to.be.lte(lastMatchedOrderPrice);
+        };
+        const submitedOrder = await ob.submit(order);
+        expect(submitedOrder).to.be.true;
     });
 });
